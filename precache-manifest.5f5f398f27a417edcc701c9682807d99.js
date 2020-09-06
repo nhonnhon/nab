f7af92a0a9e5d4ae81b279f53390a73aaed01d67
nhonnhon <vi.nhon.53th@gamil.com>
2020-09-06 17:08:48 +0700

    Updates

---
+++ precache-manifest.5f5f398f27a417edcc701c9682807d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c6de07f90d542c1bf539e2e3cca208",
+    "url": "/nab/index.html"
+  },
+  {
+    "revision": "bc75ca1cb4120af5735d",
+    "url": "/nab/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c22b63880d5bb2c4e8eb",
+    "url": "/nab/static/css/main.c7cb345b.chunk.css"
+  },
+  {
+    "revision": "bc75ca1cb4120af5735d",
+    "url": "/nab/static/js/2.dce1d846.chunk.js"
+  },
+  {
+    "revision": "bbe42beaaf3ddb1d4056d2d4aa3ccabf",
+    "url": "/nab/static/js/2.dce1d846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22b63880d5bb2c4e8eb",
+    "url": "/nab/static/js/main.5af1eb3d.chunk.js"
+  },
+  {
+    "revision": "89ba69cbd05d14f1fe9a",
+    "url": "/nab/static/js/runtime-main.34d035d7.js"
+  }
+]);
